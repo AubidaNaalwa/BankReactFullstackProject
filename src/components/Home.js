d2eb5b62829cb956a5661ff4fb049a0cc5fa0861
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2020-12-29 01:53:32 +0200

    adding the project

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+function Home(){
+        return (
+            <div id ="home">
+               <h1>BankIt bank</h1>
+               <h1>The best bank in the world </h1>
+            </div>
+        )
+}
+
+export default Home;
